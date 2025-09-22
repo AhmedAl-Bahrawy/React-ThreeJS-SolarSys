7cd365f7a6229d130109588861069f59e293ec71
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-09-22 18:57:24 +0300

    Sure! Pl

---
+++ threejs-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ThreeScene from "./components/ThreeScene";
+
+const App = () => {
+  return (
+    <>
+      <ThreeScene />
+    </>
+  );
+};
+
+export default App;
